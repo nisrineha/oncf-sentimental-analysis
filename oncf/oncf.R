213c95f7340e70475adcb6f84509b3a8437d8183
nisrineha <nisrinehammout@gmail.com>
2019-01-09 23:47:19 +0100

    init project

--- oncf/oncf.R
+++ oncf/oncf.R
@@ -1,61 +1,83 @@
 
-oncf = read.csv("ONCF3.csv", header = TRUE, sep = ";" )
-str(oncf)
+#Extract database from Twitter, make text analytics on tweets
 
-summary(oncf)
-model = lm(Number_of_likes ~ .- Image_URL - Link - Created_Time- Text, data = oncf)
-plot(model)
-
-
-summary(model)
-
-
-
-
-
-
-
-plot(oncf$Created.Time,oncf$Username)
-plot(oncf$longitude, oncf$latitude)
-boxplot
-
-
-
-
-
-
-
+echo=TRUE
+eval=TRUE
+#------------------------------------------ Install new packages
+#install.packages("tm")
 library(tm)
+#install.packages("SnowballC")
 library(SnowballC)
-
-oncf$Username = as.factor(oncf$Username )
-corpusOncf = VCorpus(VectorSource(oncf$Username))
-corpusOncf
-corpusOncf$content
-corpusOncf[[1]]$content
-corpusOncf = tm_map(corpusOncf, tolower)
-corpusOncf$content
-corpusOncf = tm_map(corpusOncf, removePunctuation)
-corpusOncf[[1]]
+#install.packages("caTools")
+library(caTools)
+#install.packages("rpart")
+library(rpart)
+#install.packages("rpart.plot")
+library(rpart.plot)
 
 
-oncf$Location = as.factor(oncf$Location )
-corpusOncf2 = VCorpus(VectorSource(oncf$Location))
-corpusOncf2
-corpusOncf2$content
+#-------------------------------------------------------------------------------
 
-corpusOncf2[[20]]$content
-corpusOncf2 = tm_map(corpusOncf2, tolower)
-corpusOncf2$content
-corpusOncf2[[20]]
-corpusOncf2 = tm_map(corpusOncf2, removePunctuation)
-corpusOncf2$content
-corpusOncf2[[20]]
-corpusOncf2 = tm_map(corpusOncf2, removePunctuation)
-stopwords("french")[1:10]
-length(stopwords("french"))
-corpusOncf2 = tm_map(corpusOncf2, stemDocument)
-corpusOncf2[[20]]
+#export data
+oncf = read.csv("ONCF3.csv", header = TRUE, sep = ";" )
+
+#view data 
+str(oncf)
+summary(oncf)
+attach(oncf)
+summary(Number_of_likes)
+table(Number_of_likes)
+
+#--------------------------------------convert to a corpus for pre-processing 
+corpus = VCorpus(VectorSource(oncf$Text)) 
+corpus
+corpus[[1]]$content
+# Convert to lower-case
+corpus = tm_map(corpus, content_transformer(tolower))
+# Remove punctuation
+corpus = tm_map(corpus, removePunctuation)
+corpus[[3]]$content
+# Remove stopwords 
+corpus = tm_map(corpus, removeWords, c(stopwords("english")))
+# Stem document 
+corpus = tm_map(corpus, stemDocument)
+
+
+
+#------------------------------------------------Create matrix---------------------------
+frequencies = DocumentTermMatrix(corpus)
+frequencies
+# Check for sparsity
+findFreqTerms(frequencies, lowfreq=20)
+# Remove sparse terms
+sparse = removeSparseTerms(frequencies, 0.995)
+sparse
+
+# --------------------------------------------Convert to a data frame----------------
+oncfSparse = as.data.frame(as.matrix(sparse))
+colnames(oncfSparse) = make.names(colnames(oncfSparse))
+
+# -------------------------------------------Add dependent variable--------------
+oncf$Negative = as.factor(oncf$Number_of_likes <= 40)
+table(oncf$Negative)
+oncfSparse$Negative = oncf$Negative
+
+
+# ------------------------------------------------Split the data------------------------
+set.seed(12)
+split = sample.split(oncfSparse$Negative, SplitRatio = 0.6)
+trainSparse = subset(oncfSparse, split==TRUE)
+testSparse = subset(oncfSparse, split==FALSE)
+
+#----------------------------------------------Build a CART model------------------------
+
+oncfCART = rpart(Negative ~ ., data=trainSparse, method="class")
+prp(oncfCART)
+# Evaluate the performance of the model
+predictCART = predict(oncfCART, newdata=testSparse, type="class")
+table(testSparse$Negative, predictCART)
+#model's accuracy
+table(testSparse$Negative)
 
 
 
